51aafc7cd579fcd64f857ddcf192c5486d61a9b4
Roy Macdonald <macdonald.roy@gmail.com>
2020-07-26 19:39:26 -0400

    Reimplemented ofxDropdownOption inheriting from ofxBaseGui instead of ofxToggle

--- src/ofxDropdownOption.cpp
+++ src/ofxDropdownOption.cpp
@@ -1,4 +1,164 @@
 #include "ofxDropdownOption.hpp"
+#include "ofGraphics.h"
+using namespace std;
+
+ofxDropdownOption::ofxDropdownOption(ofParameter<bool> _bVal, float width, float height){
+	setup(_bVal,width,height);
+}
+
+ofxDropdownOption::~ofxDropdownOption(){
+	value.removeListener(this,&ofxDropdownOption::valueChanged);
+}
+
+ofxDropdownOption * ofxDropdownOption::setup(ofParameter<bool> _bVal, float width, float height){
+	b.x = 0;
+	b.y = 0;
+	b.width = width;
+	b.height = height;
+	bGuiActive = false;
+	value.makeReferenceTo(_bVal);
+//	checkboxRect.set(1, 1, b.height - 2, b.height - 2);
+
+	value.addListener(this,&ofxDropdownOption::valueChanged);
+	registerMouseEvents();
+	setNeedsRedraw();
+
+	return this;
+
+}
+
+ofxDropdownOption * ofxDropdownOption::setup(const std::string& toggleName, bool _bVal, float width, float height){
+	value.set(toggleName,_bVal);
+	return setup(value,width,height);
+}
+
+
+bool ofxDropdownOption::mouseMoved(ofMouseEventArgs & args){
+	if(isGuiDrawing() && b.inside(args)){
+		bIsOver = true;
+//		return true;
+	}else{
+		bIsOver = false;
+	}
+	return false;
+}
+
+bool ofxDropdownOption::mousePressed(ofMouseEventArgs & args){
+	if(setValue(args.x, args.y, true)){
+		bIsOver = true;
+		return true;
+	}else{
+		bIsOver = false;
+		return false;
+	}
+}
+
+bool ofxDropdownOption::mouseDragged(ofMouseEventArgs & args){
+	if(bGuiActive && b.inside(args)){
+		bIsOver = true;
+		return true;
+	}else{
+		bIsOver = false;
+		return false;
+	}
+}
+
+bool ofxDropdownOption::mouseReleased(ofMouseEventArgs & args){
+	bool wasGuiActive = bGuiActive;
+	bGuiActive = false;
+	if(wasGuiActive && b.inside(args)){
+		bIsOver = true;
+		return true;
+	}else{
+		bIsOver = false;
+		return false;
+	}
+}
+
+void ofxDropdownOption::generateDraw(){
+	bg.clear();
+	bg.rectangle(b);
+	generateNameTextMesh(b);
+}
+
+void ofxDropdownOption::generateNameTextMesh(const ofRectangle& rect)
+{
+	std::string name;
+	auto textX = rect.x + textPadding;//+ checkboxRect.width;
+	if(getTextBoundingBox(getName(), textX, 0).getMaxX() > rect.getMaxX() - textPadding){
+		for(auto c: ofUTF8Iterator(getName())){
+			auto next = name;
+			ofUTF8Append(next, c);
+			if(getTextBoundingBox(next,textX,0).getMaxX() > rect.getMaxX() - textPadding){
+				break;
+			}else{
+				name = next;
+			}
+		}
+	}else{
+		name = getName();
+	}
+
+	textMesh = getTextMesh(name, textX, getTextVCenteredInRect(rect));
+}
+
+void ofxDropdownOption::render(){
+	if(value)
+	{
+		bg.setFillColor(thisFillColor);
+	}
+	else
+	{
+		if(bIsOver)
+		{
+			bg.setFillColor(thisBorderColor);
+		}
+		else
+		{
+			bg.setFillColor(thisBackgroundColor);
+		}
+	}
+	bg.draw();
+//	fg.draw();
+
+//	if( value ){
+//		cross.draw();
+//	}
+
+	ofColor c = ofGetStyle().color;
+	ofBlendMode blendMode = ofGetStyle().blendingMode;
+	if(blendMode!=OF_BLENDMODE_ALPHA){
+		ofEnableAlphaBlending();
+	}
+	ofSetColor(thisTextColor);
+
+	bindFontTexture();
+	textMesh.draw();
+	unbindFontTexture();
+
+	ofSetColor(c);
+	if(blendMode!=OF_BLENDMODE_ALPHA){
+		ofEnableBlendMode(blendMode);
+	}
+}
+
+//bool ofxDropdownOption::operator=(bool v){
+//	value = v;
+//	return v;
+//}
+//
+//ofxDropdownOption::operator const bool & (){
+//	return value;
+//}
+
+
+ofAbstractParameter & ofxDropdownOption::getParameter(){
+	return value;
+}
+
+void ofxDropdownOption::valueChanged(bool & value){
+    setNeedsRedraw();
+}
 
 void ofxDropdownOption::enableElement() {
 //	bGuiActive = false;
@@ -12,6 +172,14 @@ void ofxDropdownOption::disableElement() {
     setNeedsRedraw();
 }
 
+//
+//void ofxDropdownOption::setState(State state)
+//{
+//	_state = state;
+//	setNeedsRedraw();
+//}
+
+
 bool ofxDropdownOption::setValue(float mx, float my, bool bCheck){
     
     if( !isGuiDrawing() ){
@@ -23,7 +191,6 @@ bool ofxDropdownOption::setValue(float mx, float my, bool bCheck){
             bGuiActive = true;
         }else{
             bGuiActive = false;
-            
         }
     }
     if( bGuiActive ){
--- src/ofxDropdownOption.hpp
+++ src/ofxDropdownOption.hpp
@@ -2,13 +2,65 @@
 
 #include "ofxToggle.h"
 
-class ofxDropdownOption : public ofxToggle {
+class ofxDropdownOption : public ofxBaseGui{
   
 public:
+	ofxDropdownOption(){}
+	~ofxDropdownOption();
+	
 	void enableElement();
     void disableElement();
-    
+
+	ofxDropdownOption(ofParameter<bool> _bVal, float width = defaultWidth, float height = defaultHeight);
+	ofxDropdownOption * setup(ofParameter<bool> _bVal, float width = defaultWidth, float height = defaultHeight);
+	ofxDropdownOption * setup(const std::string& toggleName, bool _bVal, float width = defaultWidth, float height = defaultHeight);
+	
+
+	virtual bool mouseMoved(ofMouseEventArgs & args);
+	virtual bool mousePressed(ofMouseEventArgs & args);
+	virtual bool mouseDragged(ofMouseEventArgs & args);
+	virtual bool mouseReleased(ofMouseEventArgs & args);
+	virtual bool mouseScrolled(ofMouseEventArgs & args){return false;}
+	
+
+	template<class ListenerClass, typename ListenerMethod>
+	void addListener(ListenerClass * listener, ListenerMethod method){
+		value.addListener(listener,method);
+	}
+
+	template<class ListenerClass, typename ListenerMethod>
+	void removeListener(ListenerClass * listener, ListenerMethod method){
+		value.removeListener(listener,method);
+	}
+
+
+
+//	bool operator=(bool v);
+//	operator const bool & ();
+
+	virtual ofAbstractParameter & getParameter();
+
 protected:
-    bool setValue(float mx, float my, bool bCheck);
-    
+	void generateNameTextMesh(const ofRectangle& rect);
+	
+	
+//	enum State {
+//		UNSELECTED=0,
+//		OVER,
+//		SELECTED
+//	}_state;
+//
+//	void setState(State state);
+//
+	virtual void render();
+//	ofRectangle checkboxRect;
+	ofParameter<bool> value;
+	bool bGuiActive;
+	bool bIsOver = false;
+	
+	bool setValue(float mx, float my, bool bCheck);
+	void generateDraw();
+	void valueChanged(bool & value);
+	ofPath bg;//,fg;//,cross;
+	ofVboMesh textMesh;
 };
