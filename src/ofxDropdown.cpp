690c874021beb71d3577f84c0c13d0828377f478
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2022-12-18 21:09:12 +0100

    Add files via upload

--- src/ofxDropdown.cpp
+++ src/ofxDropdown.cpp
@@ -526,27 +526,16 @@ bool ofxDropdown_<T>::mouseReleased(ofMouseEventArgs & args){
 //--------------------------------------------------------------
 template<class T>
 bool ofxDropdown_<T>::mousePressed(ofMouseEventArgs & args){
-	if(isShowingDropdown()){
-		group.mousePressed(args);
-		return true;
-	}
+    if(setValue(args.x, args.y, true)){
+        return true;
+    }
 	return false;
 }
 //--------------------------------------------------------------
 template<class T>
 bool ofxDropdown_<T>::mouseMoved(ofMouseEventArgs & args){
-	if(ofxDropdownOption::mouseMoved(args) && !isShowingDropdown()){
-		showDropdown();
+	if(ofxDropdownOption::mouseMoved(args)){
 		return true;
-	}
-	if(isShowingDropdown()){
-		if(group.mouseMoved(args)){
-			return true;
-		}
-		if(!b.inside(args.x-1, args.y)){
-			hideDropdown();
-			return true;
-		}
 	}
 	return false;
 }
